f9dc32f5a20c811532d6288d8f65c1d76fa51ba5
Prashun Kumar <54842624+prashun07@users.noreply.github.com>
2022-10-14 19:10:50 +0530

    Fixed RightMenu Displaying bug

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,16 @@
 import React from "react";
-import { } from "@mui/material";
-import { BarChart, SearchRounded, ShoppingCartRounded } from "@mui/icons-material";
-import { useEffect } from "react";
-// For header of the Web page 
+import {} from "@mui/material";
+import {
+  BarChart,
+  SearchRounded,
+  ShoppingCartRounded,
+} from "@mui/icons-material";
+// For header of the Web page
 function Header() {
-  //Whenever you click on the top right toggleMenu the Debit card page will appear
-  useEffect(() => {
-    const toggleIcon = document.querySelector(".toggleMenu");
-    toggleIcon.addEventListener("click", () => {
-      // const a=
-      return document.querySelector(".rightMenu").classList.toggle("active");
-      // console.log(a);
-    });
-  }, []);
+  const toggle=()=>{
+    const rightMenuDisplay=document.querySelector(".rightMenu").classList.toggle("active");
+    return rightMenuDisplay;
+  }
   return (
     <header>
       <img
@@ -41,7 +39,7 @@ function Header() {
         <h2 className="userName">Prashun</h2>
       </div>
       <div className="toggleMenu">
-        <BarChart className="toggleIcon" />
+        <BarChart className="toggleIcon"  onClick={toggle}/>
       </div>
     </header>
   );
